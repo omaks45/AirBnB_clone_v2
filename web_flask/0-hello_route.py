72e8cf5fd3544d2e5414349b0f44ae0450d6efee
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-03-08 02:53:19 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask("__name__")
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def airbnb_onepage():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
