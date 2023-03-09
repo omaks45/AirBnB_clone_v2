bd5b536fcec713111644ad658c7f49cb3009d3d9
omaks45 <danielgozie5>
2023-03-09 14:45:19 +0000

    complete

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,15 @@ from flask import Flask
 app = Flask("__name__")
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     """Return a given string"""
     return ("Hello HBNB!")
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def airbnb_onepage():
+    """Return a given string"""
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
---
+++ web_flask/wsgi.py
@@ -0,0 +1,4 @@
+from 0-hello_route import app
+
+if __name__ == "__main__":
+    app.run()
